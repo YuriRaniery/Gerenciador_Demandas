c070afd5d942d096e190623d17414eb62dce1efb
devyuri03 <yuriferreiracontato2006@gmail.com>
2026-05-19 00:10:57 -0300

    Resolvendo erro no SecuriyConfig

--- src/main/java/com/feras/Gerenciador_Demandas/config/SecurityConfig.java
+++ src/main/java/com/feras/Gerenciador_Demandas/config/SecurityConfig.java
@@ -1,30 +1,37 @@
 package com.feras.Gerenciador_Demandas.config;
 
-import com.feras.Gerenciador_Demandas.service.UserService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-@RequiredArgsConstructor
 public class SecurityConfig {
-
-    private final UserService userService;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+
                 .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/usuario/cadastrar").permitAll()
-                        .anyRequest().authenticated()
+
+
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
-                .httpBasic(Customizer.withDefaults());
+
+
+                .authorizeHttpRequests(auth -> auth
+
+                        .requestMatchers(org.springframework.http.HttpMethod.POST, "/usuarios/cadastrar").permitAll()
+
+                        .requestMatchers("/usuarios/**").permitAll()
+
+
+                        .anyRequest().authenticated()
+                );
 
         return http.build();
     }
