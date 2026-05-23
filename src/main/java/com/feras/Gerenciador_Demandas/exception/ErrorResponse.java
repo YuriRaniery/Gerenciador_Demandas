f941aa47c735f2c9b3a07e27160d2528ac2cc3b7
Yuri Raniery <yuriranieryfut8@live.com>
2026-05-22 21:34:49 -0300

    ErrorResponse e ResourceNotFoundException

---
+++ src/main/java/com/feras/Gerenciador_Demandas/exception/ErrorResponse.java
@@ -0,0 +1,23 @@
+package com.feras.Gerenciador_Demandas.exception;
+
+import java.time.LocalDateTime;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponse {
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
+    private LocalDateTime timestamp;
+
+    private Integer status;
+    private String error;
+    private String message;
+    private String path;
+}
