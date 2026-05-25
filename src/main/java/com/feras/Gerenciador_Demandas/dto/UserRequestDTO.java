9817d4f58d1315475695096d5ba5843d04938b56
Yuri Raniery <yuriranieryfut8@live.com>
2026-05-25 01:42:33 -0300

    Criação do UserRequestDTO para validação dos usuários e não ficar direto no Controller

---
+++ src/main/java/com/feras/Gerenciador_Demandas/dto/UserRequestDTO.java
@@ -0,0 +1,21 @@
+package com.feras.Gerenciador_Demandas.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class UserRequestDTO {
+
+    @NotBlank(message = "O nome é obrigatório.")
+    private String nome;
+
+    @NotBlank(message = "O e-mail é obrigatório.")
+    @Email(message = "Formato de e-mail inválido.")
+    private String email;
+
+    @NotBlank(message = "A senha é obrigatória.")
+    @Size(min = 6, message = "A senha deve ter no mínimo 6 caracteres.")
+    private String senha;
+}
