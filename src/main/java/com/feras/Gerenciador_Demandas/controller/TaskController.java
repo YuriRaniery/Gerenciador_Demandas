fceca2cff90404fcfaa089707240b250ab079e82
José Ramos <joseramosdossantosjunior@gmail.com>
2026-05-31 13:48:19 -0300

    Criando, buscando, atualizando, deletando Tasks

--- src/main/java/com/feras/Gerenciador_Demandas/controller/TaskController.java
+++ src/main/java/com/feras/Gerenciador_Demandas/controller/TaskController.java
@@ -3,7 +3,6 @@ package com.feras.Gerenciador_Demandas.controller;
 import com.feras.Gerenciador_Demandas.dto.TaskRequestDTO;
 import com.feras.Gerenciador_Demandas.dto.TaskResponseDTO;
 import com.feras.Gerenciador_Demandas.model.Tasks;
-import com.feras.Gerenciador_Demandas.repository.TaskRepository;
 import com.feras.Gerenciador_Demandas.service.TaskService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -12,8 +11,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/task")
